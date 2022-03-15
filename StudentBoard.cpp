4169c00d06fc111bde3e24cccbf15916aea44d1b
Zach Veenstra <zach.veen.gr@gmail.com>
2022-03-15 00:12:28 -0400

    Game is working.

--- StudentBoard.cpp
+++ StudentBoard.cpp
@@ -4,54 +4,103 @@
 #include <algorithm>
 #include <iostream>
 
+
+/**
+ * Construct a new Board object.
+ * 
+ */
 Board::Board(){
+    // The grid is a square as tall as HEIGHT and as wide as WIDTH.
     grid = new int[WIDTH * HEIGHT];
+
+    // The board is initially invisible.
     visible = false;
 }
 
+
+/**
+ * Copy constructor. Makes a deep copy of the contents of other and 
+ * assigns them to this.
+ * 
+ * @param other The board whose contents will be copied.
+ */
 Board::Board(const Board& other){
-    // this->grid = new int[WIDTH * HEIGHT];
-    // *this->grid = *(other.grid);
-    this->grid = other.grid;
+
+    // A new grid is created for this.
+    this->grid = new int[WIDTH * HEIGHT];
+
+    // Loop through and copy the contents of other's grid into this.
+    for (int i = 0; i < WIDTH * HEIGHT; ++i) {
+        this->grid[i] = other.grid[i];
+    }
+
+    // Copy the visibility of other into this.
     this->visible = other.visible;
 }
 
+
 /**
- * Copy Operator=
- * Pass object in, steal its data and return itself
+ * Copy Operator=: 
+ * Overloads the assignment operator so that the contents of other will
+ * be deeply copied into an existing board.
  * 
- * @param other 
- * @return Board& 
+ * @param other The board whose contents will be copied.
+ * @return Board& - The existing board that inherits the contents of 
+ *                  other.
  */
 Board& Board::operator=(const Board& other){
+    /* This uses the copy-and-swap implementation. Other is an existing
+    board that this board is being set equal to. We want all the
+    elements of this to be equal to other without occupying the same 
+    memory. In order to do this, we first create a copy of other, then
+    swap the values of the variables into this board, destruct the copy,
+    and then return this. This will make sure this boards' variables
+    have the same values as other. */
 
+    // Create a deep copy of other using the copy constructor.
     Board* copy = new Board(other);
 
+    // Swap the variables of copy into this.
     std::swap(this->visible, copy->visible);
     std::swap(this->grid, copy->grid);
 
-
-    // this->grid = other.grid;
-    // this->visible = other.visible; // BAD BAD BAD
-
-    // Board* b = new Board();
-    // Board* c = new Board();
-    // std::swap(b->grid, c->grid);
-    // std::swap(b->visible, c->visible);
+    // Destruct copy.
+    copy->~Board();
 
     return *this;
 }
 
+
+/**
+ * Destroys the Board object.
+ * 
+ */
 Board::~Board(){
     delete(grid);
 }
 
+
+/**
+ * Sets whether the board is visible or not.
+ * 
+ * @param v True for visible, false for invisible.
+ */
 void Board::setVisible(bool v){
     this->visible = v;
 }
 
+
+/**
+ * Overloads the [] operator so you can select a column number in grid.
+ * 
+ * @param index The column number.
+ * @return int& 
+ */
 int& Board::Internal::operator[](int index){
+
+    // If the index goes outside the board.
     if(index >= WIDTH){
+        // Throw an exception.
         throw std::out_of_range(std::to_string(index) + 
             " is greater value than or equal to grid width of " + 
             std::to_string(WIDTH));
@@ -59,8 +108,17 @@ int& Board::Internal::operator[](int index){
     return _grid[index];
 }
 
+/**
+ * Overloads the [] operator so you can selecta a row number in grid.
+ * 
+ * @param index The row number.
+ * @return Board::Internal 
+ */
 Board::Internal Board::operator[](int index){
+
+    // If the index goes outside the board.
     if(index >= HEIGHT){
+        // Throw an exception
         throw std::out_of_range(std::to_string(index) + 
             " is greater value than or equal to grid height of " + 
             std::to_string(HEIGHT));
@@ -68,62 +126,121 @@ Board::Internal Board::operator[](int index){
     return Board::Internal(grid+(index * WIDTH));
 }
 
+
+/**
+ * Overloads the << operator so the board can be put on the stream with 
+ * a visual representation.
+ * 
+ * @param os The outstream.
+ * @param b The board being streamed.
+ * @return os The outstream.
+ */
 std::ostream& operator<<(std::ostream& os, Board const& b){
+
+    // The current value of the space.
+    char space;
+
+    // If the board is visible, we want to see all the ships.
     if (b.visible) {
-        std::cout << " |-------------------- YOUR BOARD --------------------|\n"
+        os << " |-------------------- YOUR BOARD --------------------|\n"
                      "        0        1        2        3        4        5        6        7        8        9  \n"
                      "--------------------------------------------------------------------------------------------\n";
-        for (int i = 0; i < WIDTH; ++i) {
-            std::cout << i << " |     ";
-            for (int j = 0; j < HEIGHT; ++j) {
-                std::cout << char(b.grid[(i * WIDTH) + j]) << "        ";
-                if (char(b.grid[(i * WIDTH) + j]) == EMPTY) {
-                    std::cout << " ";
+        
+        // loop through the number of rows
+        for (int i = 0; i < HEIGHT; ++i) {
+            os << i << " |     ";
+            
+            // loop through the number of columns
+            for (int j = 0; j < WIDTH; ++j) {
+                // i * width determines which row, j determines the column
+                space = char(b.grid[(i * WIDTH) + j]); 
+                os << space << "        ";
+                // if the space is empty, we print a space.
+                if (space == EMPTY) {
+                    os << " ";
                 }
             }
-            std::cout << "\n";
+            // End with a new line.
+            os << "\n";
         }
 
     }
+
+    // If the board is invisible, we only show hits and misses.
     else {
-        std::cout << " |-------------------- COMPUTER BOARD --------------------|\n"
+        os << " |-------------------- COMPUTER BOARD --------------------|\n"
                      "        0        1        2        3        4        5        6        7        8        9  \n"
                      "--------------------------------------------------------------------------------------------\n";
-        for (int i = 0; i < WIDTH; ++i) {
-            std::cout << i << " |     ";
-            for (int j = 0; j < HEIGHT; ++j) {
-                if (char(b.grid[(i * WIDTH) + j]) != HIT &&
-                    char(b.grid[(i * WIDTH) + j]) != MISS) { 
-                    std::cout << "         ";
+        
+        // loop through rows
+        for (int i = 0; i < HEIGHT; ++i) {
+            os << i << " |     ";
+
+            // loop through columns
+            for (int j = 0; j < WIDTH; ++j) {
+                // i * width determines which row, j determines the column
+                space = char(b.grid[(i * WIDTH) + j]);
+                // If the space is a ship we don't print it.
+                if (space != HIT &&
+                    space != MISS) { 
+                    os << "         ";
                 }
-                if (char(b.grid[(i * WIDTH) + j]) == HIT ||
-                    char(b.grid[(i * WIDTH) + j]) == MISS) {
-                    std::cout << char(b.grid[(i * WIDTH) + j]) << "        ";
+                // We do print hits and misses.
+                if (space == HIT ||
+                    space == MISS) {
+                    os << space << "        ";
                 }
             }
-            std::cout << "\n";
+            // End with a new line.
+            os << "\n";
         }
     }
+    // Give back the outstream.
     return os;
 }
 
+
+/**
+ * Counts the number of hits on the board.
+ * 
+ * @return Count - The number of hits.
+ */
 int Board::count() const{
-    int count = 0;
+
+    // Start hits out at zero.
+    int hits = 0;
+
+    // Loop through the board.
     for (int i = 0; i < WIDTH * HEIGHT; ++i) {
+        // If we find a hit:
         if (grid[i] == HIT) {
-            count++;
+            // Increment hits.
+            hits++;
         }
     }
-    return count;
+    return hits;
 }
 
 /**
- * Overloads < to compare boards
+ * Overloads < to compare boards. A board is less than another if it has
+ * less hits on it.
  * 
  * @param other 
  * @return true 
  * @return false 
  */
 bool Board::operator< (const Board& other){
-    return false;
+
+    bool result;
+
+    // if there are less hits on this than other, it is less than it.
+    if (this->count() < other.count()) {
+        result = true;
+    }
+    // If not, other is less than this.
+    else {
+        result = false;
+    }
+
+    return result;
 }
--- StudentGame.cpp
+++ StudentGame.cpp
@@ -7,21 +7,27 @@
 #include <ctime>
 
 /**
- * Constructor will create the ships vector and add ships to it.
+ * Creates the ships vector, player, and computer boards.
  */
 Game::Game(){
+
+	// These are the 5 ships that can be placed on the board.
 	Ship* carrier = new Ship(5,"Carrier",'C');
 	Ship* battlesip = new Ship(4,"Battleship",'B');
 	Ship* destroyer = new Ship(3,"Destroyer",'D');
 	Ship* submarine = new Ship(3,"Submarine",'S');
 	Ship* patrolBoat = new Ship(2,"Patrol Boat",'P');
+
+	// We add them to the ships vector so they can be accessed later.
 	ships = {*carrier, *battlesip, *destroyer,
 			 *submarine, *patrolBoat};
 
+	// Create a new board and assign it to player.
 	Board* p = new Board();
 	player = *p;
 	player.setVisible(true);
 
+	// Create a new board and assign it to computer.
 	Board* c = new Board();
 	computer = *c;
 	computer.setVisible(false);
@@ -35,7 +41,16 @@ void Game::hello(){
 	// displays the welcome message
 	std::cout << "|--------------- BATTLESHIP ---------------|\n\n"
    		  "You will play versus Admiral Hopper, the greatest of\n"
-   		  "computer players! Place your ships, and good luck to you!\n" << std::endl;
+   		  "computer players! Place your ships, and good luck to you!\n" << 
+			 std::endl;
+
+	// prints the different pieces
+	std::cout << "The pieces are:\n\n"
+   		  "\t" << ships.at(0) << "\n" 
+   		  "\t" << ships.at(1) << "\n"
+		  "\t" << ships.at(2) << "\n"
+          "\t" << ships.at(3) << "\n"
+          "\t" << ships.at(4) << std::endl;
 }
 
 
@@ -43,6 +58,7 @@ void Game::hello(){
  * Begin Game lets user and then computer setup boards then calls run()
  */
 void Game::beginGame(){
+
 	// greets the player
 	hello();
 
@@ -60,24 +76,22 @@ void Game::beginGame(){
  * Handle the human placing ships.
  */
 void Game::placeShips(){
+	// Coordinates for ship placement.
 	int x; 
 	int y;
-	int intDirection;
-	Direction direction;
-	Board* b = new Board();
-	b->setVisible(true);
-
-
-	// prints the different pieces
-	std::cout << "The pieces are:\n\n"
-   		  "\t" << ships.at(0) << "\n" 
-   		  "\t" << ships.at(1) << "\n"
-		  "\t" << ships.at(2) << "\n"
-          "\t" << ships.at(3) << "\n"
-          "\t" << ships.at(4) << std::endl;
-
 	
-	for (int i = 0; i < 5; ++i) {
+	// The number of ships that can be placed.
+	const int numShips = 5;
+
+	// The direction of the ship. 0 for horizontal, 1 for vertical.
+	int intDirection;
+
+	// The direction to be given to place().
+	Direction direction;
+
+	// Loop through the ships
+	for (int i = 0; i < numShips; ++i) {
+								      				// This prints the ship being placed
 		std::cout << "Where do you wish to place the " << ships.at(i) << 
 					 "?" << std::endl;
 
@@ -85,97 +99,128 @@ void Game::placeShips(){
 
 		std::cout << "Horizontally or Vertically? (0 or 1)" << std::endl;
 
-		std::cin >> intDirection;
+		std::cin >> intDirection; // Accepts an integer for the direction
 
 		std::cout << "Attempting to place the " << ships.at(i).getName() << 
 					 " at " << x << ", " << y;
+
+		// If the desired direction is horizontal.
 		if(intDirection == 0) {
 			std::cout << " horizontally." << std::endl;
 			direction = HORIZONTAL;
 		}
-		else {
+		// If the desired direction is vertical.
+		else if (intDirection == 1) {
 			std::cout << " vertically." << std::endl;
 			direction = VERTICAL;
 		}
+		// If there was an incorrect entry.
+		else {
+			std::cout << "\n\n\n\n\nIncorrect entry, try again." <<
+						 std::endl;
+		}
 
-		if (!place(x,y,direction,ships.at(i),*b)) {
-			i = i - 1; // Causes the for-loop to enter the current iteration again.
+		// Check if the direction entry was acceptable and attempt a
+		// ship placement.
+		if (intDirection <= 1 && !place(x,y,direction,ships.at(i),player)) {
+			// Causes the for-loop to enter the current iteration again.
+			i = i - 1; 
 		}
 			
-		std::cout << *b << std::endl; // prints the board after placement
+		std::cout << player << std::endl; // prints the board after placement
 		
-		player = *b;
 	}
 }
 
 /**
- * Handle the computer placing ships.
+ * Handle the computer placing ships. It picks random places based on
+ * the clock time of the machine.
  */
 void Game::placeShipsPC(){
-	// Board* b = new Board();
-	// b->setVisible(true);
-
-	// int x = std::clock() % 10;
-	// int y = std::clock() % 10;
-	// int intDirection = clock() % 2;
-	// Direction direction;
-
-	// if(intDirection == 0) {
-	// 		direction = HORIZONTAL;
-	// }
-	// else {
-	// 	direction = VERTICAL;
-	// }
+	// Coordinates for ship placement.
+	int x; 
+	int y;
 	
-	// for (int i = 0; i < 5; ++i){
-	// 	if (!place(x,y,direction,ships.at(i),*b)) {
-	// 		std::cout << *b << std::endl;
-	// 		std::cout << i << x << y << intDirection << std::endl;
-	// 		i = i - 1; // Causes the for-loop to enter the current iteration again.
-	// 	}
-	// 	//std::cout << *b << std::endl;
-	// }
+	// The number of ships that can be placed.
+	const int numShips = 5;
 
-	// computer = *b;
-	place(9,1,HORIZONTAL,ships.at(0),computer);
-	place(1,2,VERTICAL,ships.at(1),computer);
-	place(3,6,HORIZONTAL,ships.at(2),computer);
-	place(5,7,VERTICAL,ships.at(3),computer);
-	place(4,4,VERTICAL,ships.at(4),computer);
-	std::cout << computer << std::endl;
-	
+	// The direction of the ship. 0 for horizontal, 1 for vertical.
+	int intDirection;
+
+	// The direction to be given to place().
+	Direction direction;
+
+	std::cout << "\n\n\nThis is the output of HOPPER placing her ships."
+				 "\n-----------------------------------------------" <<
+				 std::endl;
+
+	// Loop through all the ships.
+	for (int i = 0; i < numShips; ++i){
+		
+		/* This takes the last digit of the machines current clock time.
+		This gives a random number for the coordinates. */
+		x = clock() % 10;
+		y = clock() % 10;
+
+		// This will use that same clock time and get either a 0 or a 1.
+		int intDirection = clock() % 2;
+
+		// If it is 0, the direction will be horizontal.
+		if(intDirection == 0) {
+			direction = HORIZONTAL;
+		}
+		// If it's 1, the direction will be vertical.
+		else {
+			direction = VERTICAL;
+		}
+		// Attempt to place the ship.
+		if (!place(x,y,direction,ships.at(i),computer)) {
+			i = i - 1; // Causes the for-loop to enter the current iteration again.
+		}
+	}
+	std::cout << "\n-----------------------------------------------" <<
+				 std::endl;
 }
 
 /**
  * Helper method that checks if it is safe to put a ship
- * at a particular spot with a particular direction.
+ * at a particular spot with a particular direction. A ship must not 
+ * collide with another ship, and must be fully on the board.
  */
 bool Game::place(const int& x, const int& y, Direction d, const Ship& s, Board& b){
-	Board* copycopy = new Board(b);
-	Board &copy = *copycopy;
-	// this is bad, but it works...
-	// Board copy(b);
 
+	/* Use the copy constructor to create a copy of b. We need this just 
+	in case the placement goes wrong. */
+	Board copy(b);
 
 	bool result = false;
 
 	try {
+		// Loop through the amount of spaces the ship takes up
 		for (int i = 0; i < s.getSpaces(); ++i) {
+			// If we want to place horizontally.
 			if (d == HORIZONTAL) {
-				if (b[x][y + i] == EMPTY) {
+				// If the space is empty:
+				if (copy[x][y + i] == EMPTY) {
+					// Place the character representation of the ship.
 					copy[x][y + i] = s.getChr();
 					result = true;
 				}
+				// If the space is occupied.
 				else {
 					std::cout << "That intersects with another ship. Try again.\n";
 					return false;
 				}
 			}
+			// If we want to place the ship vertically.
 			else {
-				if (b[x + i][y] == EMPTY) {
+				// If the space is empty
+				if (copy[x + i][y] == EMPTY) {
+					// Place the character representation of the ship.
 					copy[x + i][y] = s.getChr();
 					result = true;
 				}
+				// If the space is occupied.
 				else {
 					std::cout << "That intersects with another ship. Try again.\n";
 					return false;
@@ -183,87 +228,142 @@ bool Game::place(const int& x, const int& y, Direction d, const Ship& s, Board& 
 			}
 		}
 	}
+	// If the space is outside of the board, we will throw an exception.
 	catch (std::out_of_range) {
 		std::cout << "That is out of bounds of the board. Try again.\n";
 		return false;
 	}
 
+	// If all the spaces we place the ship in were valid.
 	if(result) {
+		// Set the original board equal to the copy we made of it.
 		b = copy;
 	}
 
+	// True upon success.
 	return result;
 }
 
+
 /**
- * Call human turn/computer turn until someone wins.
+ * Calls human turn/computer turn until someone wins. It also displays
+ * who is currently winning
  */
 void Game::run(){
+
+	// The total amount of spaces the ships take up on a board.
 	const int totalSpaces = 17;
+
+	// While both boards have still have ships, keep the game going.
 	while (player.count() != totalSpaces && 
 		   computer.count() != totalSpaces) {
+		
+		// The human attacks.
 		humanTurn();
+
+		// The computer attacks.
 		computerTurn();
+
+		// Print the boards.
 		std::cout << player << std::endl;
 		std::cout << computer << std::endl;
-		if (computer.count() < player.count()) {
+
+		// Print who is winning.
+		if (computer < player) {
 			std::cout << "HOPPER IS WINNING" << std::endl;
 		}
 		else {
 			std::cout << "YOU ARE WINNING" << std::endl;
 		}
+
+		// Print the score.
 		std::cout << "Score: " << computer.count() << " to " << 
 					 player.count() << "." << std::endl;
 	}
-	if (computer.count() < player.count()) {
+
+	// If the computer has less hits on their board, they win.
+	if (computer < player) {
 		std::cout << "HOPPER WON!\nYOU LOSE, LOSER."<< std::endl;
 	}
+	// Otherwise the human wins.
 	else {
 		std::cout << "YOU WIN!\n" << std::endl;
 	}
 }
 
+
+/**
+ * The human picks which coordinate to attack.
+ * 
+ */
 void Game::humanTurn(){
+
+	// The coordinates on the board
 	int x;
 	int y;
+
 	std::cout << "Which coordinate do you wish to attack?" << std::endl;
+	// The human picks which x and y coordinates to attack.
 	std::cin >> x >> y;
 
+	// The value that occupies the attacked space.
 	char space = computer[x][y];
 
+	// If the value is a ship:
 	if (space != EMPTY &&
-	   (space != MISS && space != HIT)) {
+	   space != MISS && space != HIT) {
 		std::cout << "Player HIT!" << std::endl;
+		// Change the value to hit.
 		computer[x][y] = HIT;
 	}
+	// If the value is empty:
 	else if (space == EMPTY) {
 		std::cout << "Player MISS!" << std::endl;
+		// Chanve the value to miss.
 		computer[x][y] = MISS;
 	}
+	// If the value is hit or miss, it has already been attacked.
 	else {
-		std::cout << "This space has already been attacked. Try again." << 
+		std::cout << "That space has already been attacked."
+					 " Try again, stupid human." << 
 					 std::endl;
+		// Restart the stupid human's turn.
 		humanTurn();
 	}
 	
 	
 }
 
+
+/**
+ * The computer randomly picks which coordinate to attack.
+ * 
+ */
 void Game::computerTurn(){
+
+	/* This takes the last digit of the machines current clock time.
+		This gives a random number for the coordinates. */
 	int x = std::clock() % 10;
 	int y = std::clock() % 10;
 
+	// The value at the attacked space.
 	char space = player[x][y];
 
+	// If the value is a ship.
 	if (space != EMPTY &&
-	   (space != MISS && space != HIT)) {
+	   space != MISS && space != HIT) {
 		std::cout << "Computer HIT!" << std::endl;
+		// Change the value to hit.
 		player[x][y] = HIT;
 	}
+	// If the value is empty.
 	else if (space == EMPTY) {
 		std::cout << "Computer MISS!" << std::endl;
+		// Change the value to miss.
 		player[x][y] = MISS;
 	}
+	// HOPPER tried to attack a space she already had, makes sense given
+	// that she is 115 years old...
 	else {
 		computerTurn(); // the space was already attacked, retry
 	}
@@ -277,7 +377,9 @@ void Game::computerTurn(){
 int main(int argc, char** argv){
 	(void)argc;
 	(void)argv;
+	// The game instance.
 	Game g;
+	// Let the games begin!
 	g.beginGame();
 
 	return 0;
